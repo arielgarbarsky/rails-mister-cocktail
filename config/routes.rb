67287e5fec55ea3dec598ce335b51f10d741638a
ariel garbarsky <arig91@gmail.com>
2020-05-13 20:57:03 +0300

    exc 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ## COCKTAILS
+  # See all cocktails
+    # get "cocktails", to: "cocktails#index"
+
+  # Can see any cocktail and each details
+    # get "cocktails/:id", to: "cocktails#show", as: :cocktail
+
+  # Create a new cocktail
+    # get "cocktails/new", to: "cocktails#new"
+    # post "cocktails", to: "cocktails#create"
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
