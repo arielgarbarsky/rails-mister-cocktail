dce506e34030b9bf339fa68d3e3a2c4bb6808df7
ariel garbarsky <arig91@gmail.com>
2020-05-14 15:54:58 +0300

    cocktails fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  ## COCKTAILS
-  # See all cocktails
-    # get "cocktails", to: "cocktails#index"
-
-  # Can see any cocktail and each details
-    # get "cocktails/:id", to: "cocktails#show", as: :cocktail
-
-  # Create a new cocktail
-    # get "cocktails/new", to: "cocktails#new"
-    # post "cocktails", to: "cocktails#create"
+  root to: 'cocktails#index'
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
   end
